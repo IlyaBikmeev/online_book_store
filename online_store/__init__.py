af86011cf5227efc4d9f9b12561a20e5f1b4350a
Aleksandr-74 <74simakovan@gmail.com>
2022-11-05 12:15:34 +0500

    file_init
    Файл init готов

---
+++ online_store/__init__.py
@@ -0,0 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
+app = Flask(__name__)
+db = SQLAlchemy()
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+db.init_app(app)
