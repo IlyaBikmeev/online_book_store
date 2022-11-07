afdbcbe8a240c28a590da5b6f06b1d7f51ec41a9
Ilya Bikmeev <68954703+IlyaBikmeev@users.noreply.github.com>
2022-11-07 18:52:45 +0500

    Update __init__.py
    Поправил code style

--- online_store/__init__.py
+++ online_store/__init__.py
@@ -8,5 +8,8 @@ load_dotenv()
 app = Flask(__name__)
 db = SQLAlchemy()
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'SQLALCHEMY_DATABASE_URI'
+)
+
 db.init_app(app)
